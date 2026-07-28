3e71d600e415859a6d85074be9a74718241af954
NIKHIL SINGH <nikhil1220nu@gmail.com>
2026-07-28 10:27:50 +0530

    security: remove development scratch/test files containing hardcoded database connection credentials

--- server/scratch/get_admin.js
+++
@@ -1,4 +0,0 @@
-const { getTableData } = require('../config/db');
-const users = getTableData('users.xlsx');
-const admins = users.filter(u => u.role === 'admin');
-console.log('Admins found:', JSON.stringify(admins, null, 2));
--- server/scratch/get_users.js
+++
@@ -1,3 +0,0 @@
-const { getTableData } = require('../config/db');
-const users = getTableData('users.xlsx');
-console.log('All Users:', JSON.stringify(users.map(u => ({ id: u.id, email: u.email, role: u.role })), null, 2));
